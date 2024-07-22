adecc31f08f3df811c2b136af299c4a3a92e8e71
YuvrajPrashar <yuvraj1598.be21@chitkara.edu.in>
2024-07-22 16:30:34 +0530

    removed reduntant fragment

--- client/src/Components/StockInfo.jsx
+++ client/src/Components/StockInfo.jsx
@@ -17,27 +17,25 @@ const StockInfo = (props) => {
       });
   };
   return (
-    <>
-      <div className="bg-white shadow-md border-2  hover:shadow-lg rounded-3xl p-6 transition-shadow duration-300 ease-in-out my-4">
-        <div className="grid grid-cols-3 gap-5 p-3">
-          <div className="col-span-2 font-bold text-2xl text-blue-600 ">
-            {props.stockData?.stocksymbol}
-          </div>
-          <div className="justify-self-end">
-            <AddCircleOutline onClick={watchListHandler} />
-          </div>
-          <div className="col-span-3 font-semibold text-lg">
-            {props.stockData?.sector}
-          </div>
-          <div className="col-span-3 font-light text-lg">
-            {props.stockData?.stockname}
-          </div>
-          <div className="col-span-2 font-bold text-2xl">
-            ${props.stockData?.currentprice}
-          </div>
+    <div className="bg-white shadow-md border-2  hover:shadow-lg rounded-3xl p-6 transition-shadow duration-300 ease-in-out my-4">
+      <div className="grid grid-cols-3 gap-5 p-3">
+        <div className="col-span-2 font-bold text-2xl text-blue-600 ">
+          {props.stockData?.stocksymbol}
+        </div>
+        <div className="justify-self-end">
+          <AddCircleOutline onClick={watchListHandler} />
+        </div>
+        <div className="col-span-3 font-semibold text-lg">
+          {props.stockData?.sector}
+        </div>
+        <div className="col-span-3 font-light text-lg">
+          {props.stockData?.stockname}
+        </div>
+        <div className="col-span-2 font-bold text-2xl">
+          ${props.stockData?.currentprice}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
