1e2538326b987311d1fecef93bced45a27fa1386
YuvrajPrashar <yuvraj1598.be21@chitkara.edu.in>
2024-07-24 14:45:03 +0530

    chore: Update Orders page layout and styling

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -15,8 +15,8 @@ const Orders = () => {
       <div className="text-6xl text-center underline decoration-teal-600 underline-offset-8 decoration-2 font-thin">
         Orders
       </div>
-      <div className="overflow-auto">
-        <table className="  w-4/6 mx-auto text-sm text-left  text-gray-500 ">
+      <div className="h-5/6 w-4/5 overflow-auto mx-auto shadow-2xl ">
+        <table className="w-full text-sm text-left  text-gray-500 ">
           <thead className="text-xs  text-gray-700 uppercase bg-gray-50">
             <tr>
               <th scope="col" className="px-6 py-3">
