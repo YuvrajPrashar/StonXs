ce7fdcee6252fa35cc8e6c524e33731f7aafd9f0
YuvrajPrashar <yuvraj1598.be21@chitkara.edu.in>
2024-07-24 13:00:37 +0530

    chore: Add login modal functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,11 @@
+import Modal from "../Components/Modal.jsx";
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
   const [data, setData] = useState({
     username: "",
     password: "",
@@ -14,21 +17,19 @@ const Login = () => {
     axios
       .post("http://localhost:8080/login", data, { withCredentials: true })
       .then((res) => {
-        console.log("Response:", res);
-        console.log(
-          `userId: ${res.headers["userid"]}, watchlistId: ${res.headers["watchlistid"]}, portfolioId: ${res.headers["portfolioid"]}`
-        );
         localStorage.setItem("userId", res.headers["userid"]);
         localStorage.setItem("watchlistId", res.headers["watchlistid"]);
         localStorage.setItem("portfolioId", res.headers["portfolioid"]);
         navigate("/");
         window.location.reload();
+        modalMessage(res.data);
       })
       .catch((error) => {
         console.log(
           "Login Error:",
           error.response ? error.response.data : error.message
         );
+        setModalMessage(error.response ? error.response.data : error.message);
       });
   };
 
@@ -41,67 +42,72 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center">Log In</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            signInHandler();
-          }}
-          className="space-y-4"
-        >
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Enter your username"
-              onChange={inputHandler}
-              value={data.username}
-              className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              onChange={inputHandler}
-              value={data.password}
-              className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700"
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
+          <h2 className="text-2xl font-bold text-center">Log In</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              signInHandler();
+            }}
+            className="space-y-4"
           >
-            Log In
-          </button>
-        </form>
-        <div className="text-center">
-          <p className="text-sm text-gray-600">
-            Not an existing user?
-            <button className="font-medium text-blue-600 hover:text-blue-800 focus:outline-none">
-              <Link to="/signup">Sign Up</Link>
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                onChange={inputHandler}
+                value={data.username}
+                className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                onChange={inputHandler}
+                value={data.password}
+                className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700"
+            >
+              Log In
             </button>
-          </p>
+          </form>
+          <div className="text-center">
+            <p className="text-sm text-gray-600">
+              Not an existing user?
+              <button className="font-medium text-blue-600 hover:text-blue-800 focus:outline-none">
+                <Link to="/signup">Sign Up</Link>
+              </button>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      {showModal && (
+        <Modal message={modalMessage} onClose={() => setShowModal(false)} />
+      )}
+    </>
   );
 };
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,11 @@
+import Modal from "../Components/Modal.jsx";
 import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
   const [data, setData] = useState({
     fullname: "",
     username: "",
@@ -14,9 +17,13 @@ const SignUp = () => {
     axios
       .post("http://localhost:8080/signup", data)
       .then((res) => {
+        setShowModal(true);
+        setModalMessage(res.data);
         console.log(res.data);
       })
       .catch((error) => {
+        setShowModal(true);
+        setModalMessage(error.response.data);
         console.log(error);
       });
   };
@@ -30,78 +37,83 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center">Sign Up</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-          className="space-y-4"
-        >
-          <div>
-            <input
-              type="text"
-              id="fullname"
-              name="fullname"
-              placeholder="Enter your full name"
-              onChange={inputHandler}
-              value={data.fullname}
-              className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <div>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              onChange={inputHandler}
-              value={data.email}
-              className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Enter your username"
-              onChange={inputHandler}
-              value={data.username}
-              className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              onChange={inputHandler}
-              value={data.password}
-              className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <button
-            type="submit"
-            onClick={signUpHandler}
-            className="w-full py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700"
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-md rounded-lg">
+          <h2 className="text-2xl font-bold text-center">Sign Up</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            className="space-y-4"
           >
-            Sign Up
-          </button>
-        </form>
-        <div className="text-center">
-          <p className="text-sm text-gray-600">
-            Already have an account ?
-            <button className="font-medium text-blue-600 hover:text-blue-800 focus:outline-none">
-              {" "}
-              <Link to="/login">Log In</Link>{" "}
+            <div>
+              <input
+                type="text"
+                id="fullname"
+                name="fullname"
+                placeholder="Enter your full name"
+                onChange={inputHandler}
+                value={data.fullname}
+                className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+            <div>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                onChange={inputHandler}
+                value={data.email}
+                className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                onChange={inputHandler}
+                value={data.username}
+                className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                onChange={inputHandler}
+                value={data.password}
+                className="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+            </div>
+            <button
+              type="submit"
+              onClick={signUpHandler}
+              className="w-full py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700"
+            >
+              Sign Up
             </button>
-          </p>
+          </form>
+          <div className="text-center">
+            <p className="text-sm text-gray-600">
+              Already have an account ?
+              <button className="font-medium text-blue-600 hover:text-blue-800 focus:outline-none">
+                {" "}
+                <Link to="/login">Log In</Link>{" "}
+              </button>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      {showModal && (
+        <Modal message={modalMessage} onClose={() => setShowModal(false)} />
+      )}
+    </>
   );
 };
 
